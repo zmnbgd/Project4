bd541e8b6d3cbba70b8f40fbe6b4abd652f13c96
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2023-02-08 00:03:13 +0100

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Marko Zivanovic on 7.2.23..
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    //MARK: Property
+    var webView: WKWebView!
+    
+    //MARK: Method: loadView get's called before viewDidLoad
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+    let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+}
+
