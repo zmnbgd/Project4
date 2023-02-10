8c5da51fdf62d17912bf7897dac54d9f61ecd7e8
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2023-02-11 00:28:26 +0100

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
     //MARK: Property
     var webView: WKWebView!
     var progressVIew: UIProgressView!
+    var websites = ["apple.con", "hackingwithswift.com"]
     
     
     //MARK: Method: loadView get's called before viewDidLoad
@@ -36,14 +37,18 @@ class ViewController: UIViewController, WKNavigationDelegate {
         
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         
-    let url = URL(string: "https://www.hackingwithswift.com")!
+    let url = URL(string: "https://" + websites[0])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "hackingwithswift.com.com", style: .default, handler: openPage))
+        
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
+        
+//        ac.addAction(UIAlertAction(title: "hackingwithswift.com.com", style: .default, handler: openPage))
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(ac, animated: true)
@@ -57,10 +62,22 @@ class ViewController: UIViewController, WKNavigationDelegate {
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
     }
-    override class func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if keyPath == "estimatedProgress" {
             progressVIew.progress = Float(webView.estimatedProgress)
         }
+    }
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        decisionHandler(.cancel)
     }
 }
 
